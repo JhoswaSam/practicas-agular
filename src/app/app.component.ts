2c9816cae72753fd8aa5242c42b47be444db6bfc
Jhoswa Sam <jhoswasamanez@gmail.com>
2021-09-20 12:18:03 -0500

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -68,25 +68,33 @@ const P004 : producto = {
  * CONSULTAS
  */
 
-// ALMACEN 1
-A001.ingresarProducto(P001);
-A001.ingresarProducto(P002);
-A001.ingresarProducto(P003);
+// ALMACEN 1 INGRESAR PRODUCTOS
+A001.ingresarProducto(P001);//Roja
+A001.ingresarProducto(P002);//verde
+A001.ingresarProducto(P003);//amarilla
 
+// MOSTRAMOS ALMACEN
 A001.mostrarProducto();
 
-// ALMACEN 2
-
+// ALMACEN 2 INGRESAR PRODUCTOS
 A002.ingresarProducto(P001);
 A002.ingresarProducto(P001);
 A002.ingresarProducto(P001);
 
+//MOSTRANOS ALMACEN
 A002.mostrarProducto();
 
+//ACCION MOVER PRODUCTOS ENTRE ALMACEN (1) 
 A001.moverProducto(P002,A002);
 A001.moverProducto(P002,A002);
 
-A002.mostrarProducto();
+//A002.mostrarProducto();
 
+//ACCION MOVER PRODUCTOS ENTRE ALMACEN (*)
 A002.moverProductoCantidad(P001,A001,4);
 
+//MOSTRAR INVENTARIO CON CANTIDAD
+A001.mostrarInventario();
+
+//MODIFICAR INVENTARIO 
+A001.modificarInventario("R",P003,3);
--- src/app/clases.ts
+++ src/app/clases.ts
@@ -9,6 +9,7 @@ export default class Almacen{
   
   public ingresarProducto(P : producto){
     this.stock.push(P);
+    console.log("Producto ingresado correctamente.");
   }
   
   public mostrarProducto(){
@@ -44,6 +45,7 @@ export default class Almacen{
     let posicion = this.buscarProducto(codP);
     if(posicion == -1){
       console.log("Producto no habido en stock :D");
+
     }else{
       codA.ingresarProducto(this.stock[posicion])
       this.quitarProducto(codP)
@@ -58,17 +60,69 @@ export default class Almacen{
   }
 
   public mostrarInventario(){
+    let i = 0;
     console.log("******************************\n*****"+this.nombreA+"*****\n******************************");
-    
+    let inventario = new Map();
+
+    let cont = 1
+    for (let index = 0; index < this.stock.length; index++) {
+      
+      if(!inventario.has(this.stock[index].nombreP)){
+        inventario.set(this.stock[index].nombreP,1);
+      }else{
+        let found = false;
+        while(!found && i < inventario.size) {
+          if(this.stock[i].nombreP ==  getByValue(inventario,i)) {
+            cont +=1;
+            found = true;
+            inventario.set(this.stock[i].nombreP,cont);
+            } else {
+              i += 1;
+            }
+          }
+        }
+      }
+    for(let CV of inventario.entries()){
+      console.log(CV);
+    }
+  }
+
+  public modificarInventario(R : string, codP : producto, cant : number){
+    if(R == "R"){
+      for (let index = 0; index < cant; index++) {
+          let posicion = this.buscarProducto(codP);
+          if(posicion == -1){
+            console.log("Producto no habido en stock :D");
+          }else{
+            this.quitarProducto(codP);
+          }
+        }
+    }else if(R == "A"){
+        for (let index = 0; index < cant; index++) {
+          this.ingresarProducto(codP);
+        }
+    }else{
+      console.log("Accion no valida\nAcciones Validas: \n-> R : Retirar\n-> A : Agregar")
+    }
   }
 }
- 
- 
+
 // PRODUCTOS
- 
+
+
 export interface producto {    
   codigoP : string;
   nombreP  : string;
 }
- 
+
+// FUNCIONES
+
+function getByValue(map: Map<any,string>, post: number){
+ let acum = 0;
+ for (let [key, value] of map.entries()) {
+   if (acum == post)
+   return key;
+ }
+ acum += 1;
+}
  
